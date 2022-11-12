6c478e5749b1974dc0a743eecaf32ac466349c58
romanovich_anton <anton.romanovich21@gmail.com>
2022-11-12 18:07:16 +0300

    feat: add all keys to lang layout model

--- redux/slices/langSlice/interfaces.ts
+++ redux/slices/langSlice/interfaces.ts
@@ -9,5 +9,38 @@ export interface InitialStateModel {
 }
 
 export interface LangLayoutModel {
-  [K: string]: string
+  lang: string;
+  homeTitle: string;
+  homeDescription: string;
+  homeBtn: string;
+  navProfile: string;
+  navBoards: string;
+  singIn: string;
+  singUp: string;
+  register: string;
+  noAccount: string;
+  hasAccount: string;
+  name: string;
+  login: string;
+  password: string;
+  addBoardBtn: string;
+  addColumnBtn: string;
+  addTaskBtn: string;
+  createBoardHeader: string;
+  createTaskHeader: string;
+  createColumnHeader: string;
+  createNewFormTitle: string;
+  createNewFormDescr: string;
+  addBtn: string;
+  confirmBtn: string;
+  cancelBtn: string;
+  deleteBtn: string;
+  changeUser: string;
+  assignTo: string;
+  confirmation: string;
+  deleteColumn: string;
+  deleteTask: string;
+  deleteBoard: string;
+  successCreated: string;
+  successDeleted: string;
 }
