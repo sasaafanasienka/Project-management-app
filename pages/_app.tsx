b6c1d77c4dce10914b29ac2d8123890f639d6d36
romanovich_anton <anton.romanovich21@gmail.com>
2022-11-13 12:15:22 +0300

    feat: add provider wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+import getStore from '../redux/store';
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} >
-		<Head>
-			<link rel="preconnect" href="https://fonts.googleapis.com" />
-			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-			<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
-		</Head>
-	</Component>;
+	const store = getStore(pageProps.initialState);
+	return (
+		<Provider store={store}>
+			<Component {...pageProps} >
+				<Head>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+					<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+				</Head>
+			</Component>
+		</Provider>
+	);
 }
