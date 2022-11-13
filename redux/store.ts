40b11ef2c3b805c28dcaf826a3f70ee131cb25b7
romanovich_anton <anton.romanovich21@gmail.com>
2022-11-13 12:16:55 +0300

    feat: store gets preloaded state from ssr

--- redux/store.ts
+++ redux/store.ts
@@ -1,12 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { langSlice } from "./slices/langSlice";
+import { langSlice } from './slices/langSlice';
 
-export const store = configureStore({
-  reducer: {
-    lang: langSlice.reducer,
-  },
-});
+
+export default function getStore(incomingPreloadState?: undefined) {
+	return configureStore({
+		reducer: {
+			lang: langSlice.reducer,
+		},
+		preloadedState: incomingPreloadState,
+	});
+}
+
+const store = getStore();
 
 export type RootState = ReturnType<typeof store.getState>;
 
