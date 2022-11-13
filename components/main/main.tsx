f1ea32c1ea9f62cbf2ea9f184fc8b3d6a6248070
Sasha Afanasienka <afanasienka@yandex.by>
2022-11-14 00:36:10 +0300

    feature: add layout comp

--- components/main/main.tsx
+++ components/main/main.tsx
@@ -1,6 +1,13 @@
 import { FC, ReactElement } from 'react';
+import { MainPropsModel } from './interfaces';
 import StyledMain from './StyledMain';
 
-const Main: FC = (): ReactElement => <StyledMain></StyledMain>;
+const Main: FC<MainPropsModel> = (props): ReactElement => {
+	const { children } = { ...props };
+
+	return (
+		<StyledMain>{children}</StyledMain>
+	);
+};
 
 export default Main;
--- pages/boards/index.tsx
+++ pages/boards/index.tsx
@@ -1,22 +1,13 @@
 import Head from 'next/head';
 import { FC, ReactElement } from 'react';
-import Header from '../../components/header/Header';
-import Footer from '../../components/footer/Footer';
-import Aside from '../../components/aside/Aside';
-import Main from '../../components/main/Main';
-import MainContent from '../../components/main-content/MainСontent';
+import Layout from '../../components/layout/Layout';
 
 const Boards: FC = (): ReactElement => (
 	<>
 		<Head>
 			<title>Home page</title>
 		</Head>
-		<Header></Header>
-		<MainContent>
-			<Aside></Aside>
-			<Main></Main>
-		</MainContent>
-		<Footer></Footer>
+		<Layout></Layout>
 	</>
 );
 
