7ef8d3561690f075cea041520cce809a50f91869
romanovich_anton <anton.romanovich21@gmail.com>
2022-11-13 12:16:09 +0300

    fix: disable eslint params reassign

--- redux/slices/langSlice/index.ts
+++ redux/slices/langSlice/index.ts
@@ -1,25 +1,26 @@
+/* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
 import { InitialStateModel, LangOption } from './interfaces';
 import { engLayout, rusLayout } from './localization';
 
 const initialState: InitialStateModel = {
-  currentLang: LangOption.EN,
-  text: engLayout,
-}
+	currentLang: LangOption.EN,
+	text: engLayout,
+};
 
 export const langSlice = createSlice({
-  name: 'lang',
-  initialState,
-  reducers: {
-    switchToEng: (state) => {
-      state.currentLang = LangOption.EN;
-      state.text = engLayout;
-    },
-    switchToRus: (state) => {
-      state.currentLang = LangOption.RU;
-      state.text = rusLayout;
-    },
-  },
+	name: 'lang',
+	initialState,
+	reducers: {
+		switchToEng: (state) => {
+			state.currentLang = LangOption.EN;
+			state.text = engLayout;
+		},
+		switchToRus: (state) => {
+			state.currentLang = LangOption.RU;
+			state.text = rusLayout;
+		},
+	},
 });
 
 export const { switchToEng, switchToRus } = langSlice.actions;
