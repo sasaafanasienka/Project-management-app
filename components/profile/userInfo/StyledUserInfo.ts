858fe9644ed11f8ca1dd13973696de4c98ba352f
romanovich_anton <anton.romanovich21@gmail.com>
2022-11-15 12:02:21 +0300

    feat: add userInfo comp

---
+++ components/profile/userInfo/StyledUserInfo.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const StyledUserInfo = styled.div`
+  padding: 12px 18px;
+  width: 100%;
+  background-color: #fff;
+  flex: 1;
+  display: flex;
+  gap: 40px;
+  flex-direction: row;
+`;
+
+export default StyledUserInfo;
