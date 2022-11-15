6f6e907cf34b4d87903739425079273ddb4467b9
romanovich_anton <anton.romanovich21@gmail.com>
2022-11-15 12:01:32 +0300

    feat: add root profile comp and layout

---
+++ components/profile/layout/StyledProfileLayout.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const StyledProfileWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledProfileContent = styled.div`
+  padding: 16px 22px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 600px;
+  border: 1px dashed #CDCDCD;
+`;
+
