406aa675751c0ff56e8b3fc1847b3031dabea449
romanovich_anton <anton.romanovich21@gmail.com>
2022-11-15 12:02:45 +0300

    feat: add userMeta comp

---
+++ components/profile/userMeta/StyledUserMeta.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const StyledUserMeta = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex: 2;
+  color: #373737;
+
+  h3 {
+    font-size: 1.4rem;
+  }
+
+  div {
+    font-size: 1.1rem;
+  }
+`;
+
+export default StyledUserMeta;
