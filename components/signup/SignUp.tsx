05a3d7da2f92f334476eecbe1bc76a43e089ffa3
romanovich_anton <anton.romanovich21@gmail.com>
2022-11-17 00:18:45 +0300

    feat: add sign up page and comp

--- components/signup/SignUp.tsx
+++ components/signup/SignUp.tsx
@@ -1,9 +1,32 @@
 import { FC, ReactElement } from 'react';
+import { useAppSelector } from '../../redux/store';
+import BottomLink from '../validationForm/bottomLink/BottomLink';
+import FormHeader from '../validationForm/formHeader/FormHeader';
+import FormWrapper from '../validationForm/formWrapper/FormWrapper';
+import { UserUpdateFormDataModel } from '../validationForm/interfaces';
+import ValidationForm from '../validationForm/ValidationForm';
 
 const SignUp: FC = (): ReactElement => {
-	const test = 0;
+	const formActionText = useAppSelector((state) => state.lang.text.singUp);
+	const bottomLinkTxt = useAppSelector((state) => state.lang.text.hasAccount);
+	const linkTo = useAppSelector((state) => state.lang.text.singIn);
+	const headerText = useAppSelector((state) => state.lang.text.singUpGreet);
+	const headerMessage = useAppSelector((state) => state.lang.text.singUpMessage);
+
+	const onSubmit = (data: UserUpdateFormDataModel) => {
+		console.log(data);
+	};
+
 	return (
-		<>SIgnUp</>
+		<FormWrapper>
+			<FormHeader header={headerText} text={headerMessage} />
+			<ValidationForm
+				header={formActionText}
+				resetForm
+				submitBtnTxt={formActionText}
+				onSubmit={onSubmit}/>
+			<BottomLink text={bottomLinkTxt} linkTo={linkTo}/>
+		</FormWrapper>
 	);
 };
 
