54d23575d2242156415b30095d3c65ac1f5febed
romanovich_anton <anton.romanovich21@gmail.com>
2022-11-20 01:03:44 +0300

    feat: add Lang toggle btn

---
+++ components/header/langToggle/StyledLangToggle.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const StyledLangToggle = styled.button`
+  color: #fff;
+  padding: 10px;
+  background-color: inherit;
+  border-radius: 50%;
+  border: none;
+  transition: all 0.1s ease-in-out;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+`;
+
+export default StyledLangToggle;
