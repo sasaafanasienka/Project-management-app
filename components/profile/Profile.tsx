ff75cc3599c0ca942cbb1d829531aaaabb80a14a
romanovich_anton <anton.romanovich21@gmail.com>
2022-11-21 22:09:57 +0300

    feat: add modal window for deleting user

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@mui/material';
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, useState } from 'react';
 import { useAppSelector } from '../../redux/store';
+import ModalWindow from '../modal/ModalWindow';
 import FormWrapper from '../validationForm/formWrapper/FormWrapper';
 import { UserUpdateFormDataModel } from '../validationForm/interfaces';
 import ValidationForm from '../validationForm/ValidationForm';
@@ -12,19 +13,45 @@ const Profile: FC = (): ReactElement => {
 	const submitBtnTxt = useAppSelector((state) => state.lang.text.editProfile);
 	const deleteBtn = useAppSelector((state) => state.lang.text.deleteBtn);
 
+	const modalTitleLang = useAppSelector((state) => state.lang.text.deleteUser);
+	const modalDescrLang = useAppSelector((state) => state.lang.text.confirmation);
+	const cancelBtnLang = useAppSelector((state) => state.lang.text.cancelBtn);
+
+	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
 	const updateUser = (data: UserUpdateFormDataModel) => {
 		console.log(data);
 	};
 
+	const deleteUser = () => {
+		setIsModalOpen(false);
+	};
+
+	const handleModal = isModalOpen
+		? () => setIsModalOpen(false)
+		: () => setIsModalOpen(true);
+
 	return (
-		<FormWrapper>
-			<UserInfo />
-			<ValidationForm header={title} submitBtnTxt={submitBtnTxt} onSubmit={updateUser} >
-				<Button color="warning" size="large" variant='outlined'>
+		<>
+			<FormWrapper>
+				<UserInfo />
+				<ValidationForm header={title} submitBtnTxt={submitBtnTxt} onSubmit={updateUser} >
+					<Button onClick={handleModal} color="warning" size="large" variant='outlined'>
+						{deleteBtn}
+					</Button>
+				</ValidationForm>
+			</FormWrapper>
+			<ModalWindow
+				title={modalTitleLang}
+				description={modalDescrLang}
+				closeFunc={deleteUser}
+				isOpened={isModalOpen}>
+				<Button onClick={handleModal}>{cancelBtnLang}</Button>
+				<Button variant='contained' autoFocus>
 					{deleteBtn}
 				</Button>
-			</ValidationForm>
-		</FormWrapper>
+			</ModalWindow>
+		</>
 	);
 };
 
