99a769bb995d9266652364437e1837886c22e7bf
romanovich_anton <anton.romanovich21@gmail.com>
2022-11-22 00:27:56 +0300

    feat: add setIntreval for expiring token

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,35 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from '@emotion/react';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import theme from '../theme/theme';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { wrapper } from '../redux/store';
+import { useAppDispatch, wrapper } from '../redux/store';
 import Layout from '../components/layout/Layout';
+import decodeToken from '../utils/decodeToken';
+import { getUserById, logOut, restoreUserToken } from '../redux/slices/userSlice';
 
 function App({ Component, pageProps }: AppProps) {
+	const dispatch = useAppDispatch();
+	const interval = useRef<ReturnType<typeof setTimeout>>();
+
 	useEffect(() => {
-		const LS = JSON.parse(localStorage.getItem('appToken') as string);
-		console.log(LS);
-	});
+		// const LS = JSON.parse(localStorage.getItem('appToken') as string);
+		// console.log(LS);
+		if (document.cookie.replace(/(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/, '$1')) {
+			const token = document.cookie.split('token=')[1].split(';')[0];
+			const { id, login, exp } = decodeToken(token);
+			dispatch(restoreUserToken({ id, login, token }));
+			dispatch(getUserById({ id, token }));
+			interval.current = setTimeout(() => {
+				dispatch(logOut());
+			}, exp * 1000 - Date.now());
+			return () => clearTimeout(interval.current);
+		}
+	}, [dispatch]);
+
 	return (
 		<ThemeProvider theme={theme}>
 			<Layout>
