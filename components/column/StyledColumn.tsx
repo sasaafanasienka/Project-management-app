3d90d172fce0082203980e3232a2ecdbb4774e8b
Sasha Afanasienka <afanasienka@yandex.by>
2022-11-24 22:57:48 +0300

    fix: column style

--- components/column/StyledColumn.tsx
+++ components/column/StyledColumn.tsx
@@ -5,13 +5,15 @@ import theme from '../../theme/theme';
 const StyledColumn = styled.div`
   /* width: 250px; */
   flex: 0 0 250px;
-  min-height: calc(100vh - 300px);
+  min-height: max-content;
+  max-height: calc(100vh - 300px);
   border: 1px dashed ${theme.palette.secondary.light};
   border-radius: 5px;
   padding: 12px;
   display: flex;
   flex-flow: column;
   gap: 10px;
+  overflow: auto;
   & h3 {
     margin: 0;
     font-size: 16px;
