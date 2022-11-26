fd83198ccfb07887f6fbe3273a4006c54dd349ec
romanovich_anton <anton.romanovich21@gmail.com>
2022-11-26 12:26:56 +0300

    add task mock

--- components/column/Column.tsx
+++ components/column/Column.tsx
@@ -8,6 +8,40 @@ import Task from '../task/Task';
 import ModalWindow from '../modal/ModalWindow';
 import { ColumnPropsModel } from './interfaces';
 import { ModalWindowStateModel } from '../modal/interfaces';
+import { TaskModel } from '../task/interfaces';
+
+const tasksMck: TaskModel[] = [
+	{
+		_id: '1',
+		title: 'Eat',
+		order: 0,
+		boardId: '1561',
+		columnId: '16589',
+		description: 'Pasta',
+		userId: 2342343,
+		users: ['15151', '1151', '16516'],
+	},
+	{
+		_id: '2',
+		title: 'Sleep',
+		order: 0,
+		boardId: '155461',
+		columnId: '1655489',
+		description: 'React-error-boundary - is a lightweight package ready to use for this scenario with TS support built-in. This approach also lets you avoid class components that are not that popular anymore.',
+		userId: 234234345,
+		users: ['15154541', '451151', '1456516'],
+	},
+	{
+		_id: '3',
+		title: 'Train',
+		order: 0,
+		boardId: '156541',
+		columnId: '165849',
+		description: 'However, many hooks are initialized with null-ish default values, and you may wonder how to provide types. Explicitly declare the type, and use a union type:',
+		userId: 234236543,
+		users: ['1465151', '184151', '16556616'],
+	},
+];
 
 
 const Column: FC<ColumnPropsModel> = (props): ReactElement => {
@@ -36,18 +70,7 @@ const Column: FC<ColumnPropsModel> = (props): ReactElement => {
 						<DeleteIcon fontSize='small'/>
 					</IconButton>
 				</FlexBox>
-				<Task
-					title='First task'
-					description='Description'
-				/>
-				<Task
-					title='Second task'
-					description='Description'
-				/>
-				<Task
-					title='Second task'
-					description='Description'
-				/>
+				{tasksMck.map((item) => <Task key={item._id} task={item} />)}
 			</StyledColumn>
 			<ModalWindow
 				title={`Are you sure to delete the column "${title}"?`}
