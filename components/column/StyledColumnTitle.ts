cee94b2e79c65170fca7a46ede13a0fbb882f93d
romanovich_anton <anton.romanovich21@gmail.com>
2022-11-26 13:29:45 +0300

    feat: add qty of tasks

---
+++ components/column/StyledColumnTitle.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const StyledColumnTitle = styled.div`
+  h3 {
+    position: relative;
+
+    span {
+      font-size: 0.7rem;
+      text-align: center;
+      height: 15px;
+      width: 15px;
+      color: #fff;
+      background-color: #4153AF;
+      border-radius: 50%;
+      top: -10px;
+      position: absolute;
+    }
+  }
+
+`;
+
+export default StyledColumnTitle;
