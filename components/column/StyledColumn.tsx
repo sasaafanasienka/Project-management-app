943625d887e007e6c97690902e24a8e32fd753a8
romanovich_anton <anton.romanovich21@gmail.com>
2022-11-27 14:00:22 +0300

    feat: fix some styles

--- components/column/StyledColumn.tsx
+++ components/column/StyledColumn.tsx
@@ -4,11 +4,11 @@ import theme from '../../theme/theme';
 
 const StyledColumn = styled.div`
   /* width: 250px; */
-  flex: 0 0 270px;
+  flex: 0 0 320px;
   max-height: calc(100vh - 200px);
   border: 1px dashed ${theme.palette.secondary.light};
   border-radius: 5px;
-  padding: 12px;
+  padding: 24px;
   display: flex;
   flex-flow: column;
   gap: 10px;
