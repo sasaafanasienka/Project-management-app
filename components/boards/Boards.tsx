5e8e73527478174951c464449fbc661ac13710b6
romanovich_anton <anton.romanovich21@gmail.com>
2022-11-28 22:17:34 +0300

    feat: add form for new board

--- components/boards/Boards.tsx
+++ components/boards/Boards.tsx
@@ -1,44 +1,65 @@
 /* eslint-disable no-underscore-dangle */
-import { FC, ReactElement } from 'react';
+import {
+	FC, ReactElement, useEffect, useState,
+} from 'react';
+import { toast } from 'react-toastify';
+import { createBoard, getUserBoards } from '../../redux/slices/boardSlice';
+import { BoardModel } from '../../redux/slices/boardSlice/interfaces';
+import { getAllUsers } from '../../redux/slices/userSlice';
+import { useAppDispatch, useAppSelector } from '../../redux/store';
 import AddBoard from '../addBoard/AddBoard';
 import BoardLink from '../boardLink/BoardLink';
+import ModalWindow from '../modal/ModalWindow';
+import NewBoardForm from '../newBoardForm/NewBoardForm';
 import PageHeading from '../pageHeading/PageHeading';
 import FlexBox from '../styled/FlexBox';
 
-const temporaryBoardsMock = [
-	{
-		_id: '1',
-		title: 'Укусить за бочок',
-		owner: 'Серый Волк',
-		users: ['козлик', 'asdas', 'asd'],
-	},
-	{
-		_id: '2',
-		title: 'Learn Next js',
-		owner: 'Dan Abramov',
-		users: ['saa', 'asdas', 'asd'],
-	},
-	{
-		_id: '3',
-		title: 'Learn SWR',
-		owner: 'Dan Abramov',
-		users: ['saa', 'asdas', 'asd'],
-	},
-];
+const Boards: FC = (): ReactElement => {
+	const dispatch = useAppDispatch();
+	const [isModalOpened, setIsModalOpened] = useState<boolean>(false);
+	const boards = useAppSelector((state) => state.boards.boards);
 
-const Boards: FC = (): ReactElement => (
-	<>
-		<PageHeading text="Boards"></PageHeading>
-		<FlexBox justifyContent='flex-start' alignItems='stretch'>
-			{temporaryBoardsMock.map((el) => (
-				<BoardLink
-					board={el}
-					key={el._id}
-				/>
-			))}
-			<AddBoard />
-		</FlexBox>
-	</>
-);
+	const handleModal = isModalOpened
+		? () => setIsModalOpened(false)
+		: () => setIsModalOpened(true);
+
+	const handleSubmit = (formData: BoardModel) => {
+		if (formData) {
+			dispatch(createBoard(formData))
+				.unwrap()
+				.then(({ title }) => {
+					toast.success(`Board "${title}" successfully created`);
+					handleModal();
+				})
+				.catch((err) => toast.error(`An error has occured: ${err}`));
+		}
+	};
+
+	useEffect(() => {
+		dispatch(getUserBoards()).then(() => dispatch(getAllUsers()));
+	}, [dispatch]);
+
+	return (
+		<>
+			<PageHeading text="Boards"></PageHeading>
+			<FlexBox justifyContent='flex-start' alignItems='stretch'>
+				{boards.map((el) => (
+					<BoardLink
+						board={el}
+						key={el._id}
+					/>
+				))}
+				<AddBoard onClick={handleModal} />
+			</FlexBox>
+			<ModalWindow
+				title={'Create new Board'}
+				isOpened={isModalOpened}
+				closeFunc={handleModal}
+			>
+				<NewBoardForm onSubmit={handleSubmit} onClose={handleModal} />
+			</ModalWindow>
+		</>
+	);
+};
 
 export default Boards;
