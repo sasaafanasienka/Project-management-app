0d09945b2a07c1bbc5478ec129dca1fbb5488fc8
romanovich_anton <anton.romanovich21@gmail.com>
2022-12-05 21:53:09 +0300

    feat: fix validation during sigh in

--- components/validationForm/ValidationForm.tsx
+++ components/validationForm/ValidationForm.tsx
@@ -24,6 +24,10 @@ const ValidationForm: FC<ValidationFormProps> = ({
 	const name = useAppSelector((state) => state.user.user.name);
 	const login = useAppSelector((state) => state.user.user.login);
 
+	const nameLang = useAppSelector((state) => state.lang.text.name);
+	const loginLang = useAppSelector((state) => state.lang.text.login);
+	const passwordLang = useAppSelector((state) => state.lang.text.password);
+
 
 	// eslint-disable-next-line consistent-return
 	const disableSubmitBtn = () => {
@@ -38,50 +42,67 @@ const ValidationForm: FC<ValidationFormProps> = ({
 	};
 
 	return (
-		<StyledValidationForm>
-			<h2>{header}</h2>
-			<StyledForm onSubmit={handleSubmit(onSubmit)} >
-				{!isSigningIn
-				&& <TextField label="Name" variant="outlined" defaultValue={name} {...register('name', {
-					required: 'This field is required',
-					minLength: {
-						value: 2,
-						message: 'Name should consist of at least 2 chars',
-					},
-					pattern: {
-						value: /^[a-zA-Z]+$/,
-						message: 'Only letters allowed',
-					},
-				})} error={!!errors.name} helperText={errors?.name ? errors?.name.message : null}
-				/>
-				}
-				<TextField label="Login" variant="outlined" defaultValue={login} {...register('login', {
-					required: 'This field is required',
-					minLength: {
-						value: 2,
-						message: 'Login should consist of at least 2 chars',
-					},
-				})}
-				error={!!errors.login} helperText={errors?.login ? errors?.login.message : null}
-				/>
-				<TextField label="Password" variant="outlined" type={'password'} {...register('password', {
-					required: 'This field is required',
-					pattern: {
-						value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-						message: 'Password should contain at least eight characters including one letter and one number:',
-					},
-				})}
-				error={!!errors.password}
-				helperText={errors?.password ? errors?.password.message : null}
-				/>
-				<FlexBox justifyContent={children ? 'end' : 'center'}>
-					{ children }
-					<Button color="info" type="submit" size="large" variant='outlined' disabled={disableSubmitBtn()}>
-						{submitBtnTxt}
-					</Button>
-				</FlexBox>
-			</StyledForm>
-		</StyledValidationForm>
+		<>
+			{isSigningIn
+				? <StyledValidationForm>
+					<h2>{header}</h2>
+					<StyledForm onSubmit={handleSubmit(onSubmit)} >
+						<TextField label={loginLang} variant="outlined" defaultValue={login} {...register('login')} />
+						<TextField label={passwordLang} variant="outlined" type={'password'} {...register('password')}
+						/>
+						<FlexBox justifyContent={children ? 'end' : 'center'}>
+							{ children }
+							<Button color="info" type="submit" size="large" variant='outlined' disabled={disableSubmitBtn()}>
+								{submitBtnTxt}
+							</Button>
+						</FlexBox>
+					</StyledForm>
+				</StyledValidationForm>
+				: <StyledValidationForm>
+					<h2>{header}</h2>
+					<StyledForm onSubmit={handleSubmit(onSubmit)} >
+						<TextField label={nameLang} variant="outlined" defaultValue={name} {...register('name', {
+							required: 'This field is required',
+							minLength: {
+								value: 2,
+								message: 'Name should consist of at least 2 chars',
+							},
+							pattern: {
+								value: /^[a-zA-Z]+$/,
+								message: 'Only letters allowed',
+							},
+						})} error={!!errors.name} helperText={errors?.name ? errors?.name.message : null}
+						/>
+						<TextField label={loginLang} variant="outlined" defaultValue={login} {...register('login', {
+							required: 'This field is required',
+							minLength: {
+								value: 2,
+								message: 'Login should consist of at least 2 chars',
+							},
+						})}
+						error={!!errors.login} helperText={errors?.login ? errors?.login.message : null}
+						/>
+						<TextField label={passwordLang} variant="outlined" type={'password'} {...register('password', {
+							required: 'This field is required',
+							pattern: {
+								value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+								message: 'Password should contain at least eight characters including one letter and one number:',
+							},
+						})}
+						error={!!errors.password}
+						helperText={errors?.password ? errors?.password.message : null}
+						/>
+						<FlexBox justifyContent={children ? 'end' : 'center'}>
+							{ children }
+							<Button color="info" type="submit" size="large" variant='outlined' disabled={disableSubmitBtn()}>
+								{submitBtnTxt}
+							</Button>
+						</FlexBox>
+					</StyledForm>
+				</StyledValidationForm>
+			}
+		</>
+
 	);
 };
 
